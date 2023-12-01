949fba17f6394df80123c16fd5bf0c748fba108b
Victor Perez Cano <perezcanovictor@gmail.com>
2023-12-01 16:43:19 +0100

    un 0 mas

--- miner.cu
+++ miner.cu
@@ -9,7 +9,7 @@ using namespace std;
 #define TXT_BLOCK_SIZE 128
 #define FNV_PRIME 16777619
 #define OFFSET 2166136261
-#define TARGET_DIFFICULTY 0x0000FFFF
+#define TARGET_DIFFICULTY 0x00000FFF
 #define NUM_OF_THREADS 1
 
 typedef struct {
