4344997b4a99c52b870ad6f68580bcd89d1c2ddc
Victor Perez Cano <perezcanovictor@gmail.com>
2023-12-05 11:17:45 +0100

    file

--- miner.cu
+++ miner.cu
@@ -97,46 +97,69 @@ void printBlock(Block block) {
 
 int main(int argc, char *argv[]) {
 
-    char fileData[TXT_BLOCK_SIZE];
     uint32_t prevBlockHash = 0;
     Block *currentBlock, *deviceBlock;
     struct timeval t1, t2;
     double t_total;
-    int n_threads;
+    int n_threads = DEFAULT_NUM_OF_THREADS, overwrite = 0, n_blocks;
+    char *fileName = NULL, *outputFile = "blockchain.bin", fileData[TXT_BLOCK_SIZE];
+    size_t fileSize, blockSize;
+    FILE *file, *outFile;
 
-
-    if (argc < 2) {
-        printf("Usage: %s <text_file> [number_of_threads(default=128)] \n", argv[0]);
-        return -1;
+    // Parse arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--overwrite") == 0) {
+            overwrite = 1;
+        } else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--num_of_threads") == 0) {
+            if (i + 1 < argc) {
+                n_threads = atoi(argv[++i]);
+                if (n_threads > 0) continue;
+            }
+            fprintf(stderr, "Error: expected positive integer value after -n/--num_of_threads.\n");
+            fprintf(stderr, "Usage: %s <text_file> [-n/--num_of_threads number_of_threads(default=%u)] [-f/--output_file output_file(default=blockchain.bin)] [-o/--overwrite] \n", argv[0], DEFAULT_NUM_OF_THREADS);
+            exit(EXIT_FAILURE);
+        } else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--output_file") == 0) {
+            if (i + 1 < argc) {
+                outputFile = argv[++i];
+            } else {
+                fprintf(stderr, "Error: expected file name after -f/--output_file.\n");
+                fprintf(stderr, "Usage: %s <text_file> [-n/--num_of_threads number_of_threads(default=%u)] [-f/--output_file output_file(default=blockchain.bin)] [-o/--overwrite] \n", argv[0], DEFAULT_NUM_OF_THREADS);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            fileName = argv[i];
+        }
     }
-    
-    if (argc > 2) {
-        n_threads = atoi(argv[2]);
-    } else {
-        n_threads = DEFAULT_NUM_OF_THREADS;
+
+    if (fileName == NULL) {
+        fprintf(stderr, "Error: expected input file\n");
+        fprintf(stderr, "Usage: %s <text_file> [-n/--num_of_threads number_of_threads(default=%u)] [-f/--output_file output_file(default=blockchain.bin)] [-o/--overwrite] \n", argv[0], DEFAULT_NUM_OF_THREADS);
+        exit(EXIT_FAILURE);
     }
-
-    const char* fileName = argv[1];
-    FILE* file = fopen(fileName, "rb");
-
+    file = fopen(fileName, "rb");
     if (!file) {
-        fprintf(stderr, "No se pudo abrir el archivo %s\n", fileName);
+        fprintf(stderr, "Error: can't open file %s\n", fileName);
+        exit(EXIT_FAILURE);
+    }
+    // Obtener el tamaño del archivo
+    fseek(file, 0, SEEK_END);
+    fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    outFile = fopen(outputFile, overwrite ? "w+b" : "a+b");
+    if (!outFile) {
+        fprintf(stderr, "Error: can't open file %s\n", outputFile);
         exit(EXIT_FAILURE);
     }
 
-    // Obtener el tamaño del archivo
-    fseek(file, 0, SEEK_END);
-    size_t fileSize = ftell(file);
-    fseek(file, 0, SEEK_SET);
-
-    int n_blocks = (TXT_BLOCK_SIZE + fileSize - 1) / TXT_BLOCK_SIZE;
+    n_blocks = (TXT_BLOCK_SIZE + fileSize - 1) / TXT_BLOCK_SIZE;
 
     gettimeofday(&t1, NULL);
 
     for (int i=0; i < n_blocks; i++) {
 
         // Calcular el tamaño del bloque actual
-        size_t blockSize = (i == n_blocks - 1) ? (fileSize % TXT_BLOCK_SIZE) : TXT_BLOCK_SIZE;
+        blockSize = (i == n_blocks - 1) ? (fileSize % TXT_BLOCK_SIZE) : TXT_BLOCK_SIZE;
 
         // Leer el contenido del bloque actual
         memset(fileData, 0, TXT_BLOCK_SIZE);
@@ -175,6 +198,8 @@ int main(int argc, char *argv[]) {
         printf("Check hash: 0x%08x\n", hash);
         */
 
+        fwrite(currentBlock, sizeof(Block), 1, outFile);
+
         prevBlockHash = currentBlock->blockHash;
         free(currentBlock);
 
@@ -185,6 +210,7 @@ int main(int argc, char *argv[]) {
     printf("Número de hilos: %d\nTiempo total transcurrido: %f\n", n_threads, t_total);
 
     fclose(file);
+    fclose(outFile);
 
     exit(EXIT_SUCCESS);
 
